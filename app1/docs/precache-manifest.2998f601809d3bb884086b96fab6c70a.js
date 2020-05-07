ad5c7a19de00112e808d912651367da7e56031b0
himatsukura <shuranui01kotonoha.sadafun@gmail.com>
2020-05-07 17:57:32 +0900

    build結果をdocs配下に出力するように変更

---
+++ app1/docs/precache-manifest.2998f601809d3bb884086b96fab6c70a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed5bff552cc1e5ba5ec4",
+    "url": "/LearningVuex/css/about.199d8653.css"
+  },
+  {
+    "revision": "ed8c26378392ea3784f9",
+    "url": "/LearningVuex/css/app.91c77bcc.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/LearningVuex/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "ddfe96330467882a48059c3f15a21751",
+    "url": "/LearningVuex/index.html"
+  },
+  {
+    "revision": "ed5bff552cc1e5ba5ec4",
+    "url": "/LearningVuex/js/about.f6301185.js"
+  },
+  {
+    "revision": "ed8c26378392ea3784f9",
+    "url": "/LearningVuex/js/app.26d5f1b5.js"
+  },
+  {
+    "revision": "413481c8dae637829b3f",
+    "url": "/LearningVuex/js/chunk-vendors.3c6d608a.js"
+  },
+  {
+    "revision": "9d06ce5c7f50fdff022c3fcf48952b4b",
+    "url": "/LearningVuex/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/LearningVuex/robots.txt"
+  }
+]);
