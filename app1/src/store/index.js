112d5cb93acb14dcc74f0bbf53a2bee6e3fd4fb8
makoshimbo <mako3125@icloud.com>
2020-05-08 12:13:55 +0900

    入力フォームを作成

--- app1/src/store/index.js
+++ app1/src/store/index.js
@@ -2,12 +2,18 @@ import Vue from "vue";
 import Vuex from "vuex";
 
 import { counter } from "../modules/counter";
+import { todolist } from "../modules/todolist";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   strict: process.env.NODE_ENV !== "production",
   modules: {
-    counter: counter
+    counter: counter,
+    todolist: todolist
   }
 });
+
+
+
+
--- app1/src/views/ToDoList.vue
+++ app1/src/views/ToDoList.vue
@@ -1,5 +1,80 @@
 <template>
-  <div>
-      a
+  <div id="app">
+    <h1>チュートリアルToDoリスト</h1>
+
+    <table>
+    <!-- テーブルヘッダー -->
+    <thead>
+      <tr>
+        <th class="id">ID</th>
+        <th class="comment">コメント</th>
+        <th class="state">状態</th>
+        <th class="button">-</th>
+      </tr>
+    </thead>
+    <tbody>
+      <!-- todos[]の配列の要素だけ繰り返し表示 -->
+        <tr v-for="item in todos" v-bind:key="item.id">
+            <th>{{ item.id }}</th>
+            <td>{{ item.comment }}</td>
+            <td class="state">
+            <!-- 状態変更ボタンのモック -->
+            <button>{{ item.state }}</button>
+            </td>
+            <td class="button">
+            <!-- 削除ボタンのモック -->
+            <button>削除</button>
+            </td>
+        </tr>
+    </tbody>
+  </table>
+  
+
+  <p>※削除ボタンはコントロールキーを押しながらクリックして下さい</p>
+
+    <!-- 新規登録フォーム -->
+    <h2>新しい作業の追加</h2>
+    <form class="add-form" v-on:submit.prevent="doAdd">
+    <!-- コメント入力フォーム -->
+    コメント <input type="text" ref="comment">
+    <!-- 追加ボタンのモック -->
+      <button type="submit">追加</button>
+    </form>
+    
+    
   </div>
 </template>
+
+<script>
+
+//import store from '../store'
+import { mapState } from 'vuex'
+import { mapMutations } from 'vuex' 
+
+export default {
+    name: "ToDoList",
+    computed: {
+        /*todos () {
+            return store.state.todos
+        }*/
+        ...mapState([
+            "todos" // => store.state.todosをthis.todosと記述できる
+        ]),
+    },
+
+    methods:{
+        ...mapMutations({
+            doAdd: "todolist/doAdd" // => this.$store.commit('doAdd')をthis.doAdd()と記述できる
+            
+        }),
+        add(){
+            this.doAdd();
+        }
+    }
+};
+    
+</script>
+
+<style lang="scss" scoped>
+
+</style>
